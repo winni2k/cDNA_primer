4fa5fbbc69462255d75f6a30800df50d0d185c5d
Magdoll <magdoll@gmail.com>
2014-01-31 16:24:07 -0800

    added disclaimer

--- scripts/summarize_primer_info.py
+++ scripts/summarize_primer_info.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python
+#################################################################################$$
+# Copyright (c) 2011-2014, Pacific Biosciences of California, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the
+# disclaimer below) provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#  notice, this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials provided
+#  with the distribution.
+#
+#  * Neither the name of Pacific Biosciences nor the names of its
+#  contributors may be used to endorse or promote products derived
+#  from this software without specific prior written permission.
+#
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+# GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY PACIFIC
+# BIOSCIENCES AND ITS CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#################################################################################$$
+
 import os, sys
 import argparse
 from csv import DictReader
@@ -47,7 +84,7 @@ with open(input) as f:
         num_subread_53seen += see5 and see3
         num_subread_53Aseen += see5 and see3 and seeA
 
-print "------ 5' primer seen sumary ---- "
+print "------ 5' primer seen summary ---- "
 if not isCCS:
     print "Per subread: {0}/{1} ({2:.1f}%)".format(num_subread_5seen, num_subread, num_subread_5seen*100./num_subread)
 tmp = sum(any(x) for x in ZMW_5seen.itervalues())
@@ -58,7 +95,7 @@ tmp = sum((len(x)>0 and x[0]) for x in ZMW_5seen.itervalues())
 if not isCCS:
     print "Per ZMW first-pass: {0}/{1} ({2:.1f}%)".format(tmp, num_ZMW, tmp*100./num_ZMW)
 
-print "------ 3' primer seen sumary ---- "
+print "------ 3' primer seen summary ---- "
 if not isCCS:
     print "Per subread: {0}/{1} ({2:.1f}%)".format(num_subread_3seen, num_subread, num_subread_3seen*100./num_subread)
 tmp = sum(any(x) for x in ZMW_3seen.itervalues())
@@ -67,7 +104,7 @@ tmp = sum((len(x)>0 and x[0]) for x in ZMW_3seen.itervalues())
 if not isCCS:
     print "Per ZMW first-pass: {0}/{1} ({2:.1f}%)".format(tmp, num_ZMW, tmp*100./num_ZMW)
 
-print "------ 5'&3' primer seen sumary ---- "
+print "------ 5'&3' primer seen summary ---- "
 if not isCCS:
     print "Per subread: {0}/{1} ({2:.1f}%)".format(num_subread_53seen, num_subread, num_subread_53seen*100./num_subread)
 tmp = 0
@@ -82,7 +119,7 @@ if not isCCS:
     print "Per ZMW first-pass: {0}/{1} ({2:.1f}%)".format(tmp, num_ZMW, tmp*100./num_ZMW)
 
 
-print "------ 5'&3'&polyA primer seen sumary ---- "
+print "------ 5'&3'&polyA primer seen summary ---- "
 if not isCCS:
     print "Per subread: {0}/{1} ({2:.1f}%)".format(num_subread_53Aseen, num_subread, num_subread_53Aseen*100./num_subread)
 tmp = 0
