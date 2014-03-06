db8eb7f6485ddb1a701e5780b9b09de0008d03c3
Magdoll <magdoll@gmail.com>
2014-03-06 09:43:55 -0800

    setup fix for numpy error

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 #from Cython.Distutils import build_ext
+import numpy as np
 
 ext_modules = [Extension("BioReaders", ["BioReaders.c"]), \
         Extension("c_branch", ["c_branch.c"]), \
@@ -9,6 +10,7 @@ ext_modules = [Extension("BioReaders", ["BioReaders.c"]), \
 setup(
 		name = 'c_cDNA_primer',
 		ext_modules = ext_modules,
+        include_dirs = [np.get_include()],
         author_email='etseng@pacificbiosciences.com',
         author='Elizabeth Tseng'
 )
