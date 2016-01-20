8e52e8100f1ca8c9ee2994720d720214d6809cf5
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-01-19 20:05:51 -0800

    Use setuptools
    * Use setuptools instead of distutils.
    * Expect everything to be pip-installed early.
      * See wiki.
    * Ignore some stuff.
    
    Resolves #17, hopefully.

--- pbtranscript-tofu/pbtranscript/pbtools/pbtranscript/branch/C/setup2.py
+++ pbtranscript-tofu/pbtranscript/pbtools/pbtranscript/branch/C/setup2.py
@@ -1,18 +1,16 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 import numpy
 
-ext_modules = [#Extension("BioReaders", ["BioReaders.pyx"]), \
+ext_modules = [
+        #Extension("BioReaders", ["BioReaders.pyx"]),
         Extension("c_branch", ["c_branch.pyx"],
-            include_dirs=[numpy.get_include()]),\
-        Extension("modified_bx_intervals.intersection_unique", ["modified_bx_intervals/intersection_unique.pyx"])]
-
+            include_dirs=[numpy.get_include()]),
+        Extension("modified_bx_intervals.intersection_unique", ["modified_bx_intervals/intersection_unique.pyx"]),
+]
 setup(
 		name = 'c_cDNA_primer',
-        cmdclass = {'build_ext': build_ext},
+        #cmdclass = {'build_ext': build_ext},
 		ext_modules = ext_modules,
         author_email='etseng@pacificbiosciences.com',
         author='Elizabeth Tseng'
 )
-
